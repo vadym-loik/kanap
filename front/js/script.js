47917fd58804c13911bce10436e08fb471f46ea5
vadym-loik <loikfit@gmail.com>
2022-09-21 17:07:15 +0200

    Update script.js

---
+++ front/js/script.js
@@ -0,0 +1,29 @@
+//taking products from the API
+let getProducts = fetch('http://localhost:3000/api/products')
+  .then((response) => {
+    return response.json();
+  })
+  .then((value) => {
+    addProducts(value);
+  })
+  .catch((error) => {
+    console.log('Error ' + error);
+  });
+
+//select the section with the identifier
+const itemsSection = document.querySelector('#items');
+
+//a function which goes through our array of objects and adds HTML markup to the DOM
+function addProducts(articles) {
+  for (let article of articles) {
+    itemsSection.innerHTML += `
+    <a href="./product.html?id=${article._id}">
+      <article>
+          <img src="${article.imageUrl}" alt="${article.altTxt}">
+          <h3 class="productName">${article.name}</h3>
+          <p class="productDescription">${article.description}</p>
+      </article>
+  </a>
+    `;
+  }
+}
