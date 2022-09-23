09c6794a9110b35f2d080df317f8e1c59cc18895
vadym-loik <loikfit@gmail.com>
2022-09-23 18:31:32 +0200

    Update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,7 +1,36 @@
 const queryString = window.location.search;
+console.log(queryString);
 
 const urlParams = new URLSearchParams(queryString);
+console.log(urlParams.has('id'));
 
-const page_type = urlParams.get('page_type');
+const productId = urlParams.get('id');
+console.log(productId);
 
-console.log(page_type);
+fetch(`http://localhost:3000/api/products/${productId}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    renderProduct(data, document);
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log('Error ' + error);
+  });
+
+const renderProduct = (fetchProductData, document) => {
+  document.querySelector('.item__img').innerHTML =
+    '<img class="prodImg" src="' +
+    fetchProductData.imageUrl +
+    '" alt="Kanap company logo">';
+  document.querySelector('#title').innerText = fetchProductData.name;
+  document.querySelector('#price').innerText = fetchProductData.price;
+  document.querySelector('#description').innerText =
+    fetchProductData.description;
+  fetchProductData.colors.forEach((color) => {
+    document.querySelector('#colors').innerHTML +=
+      '<option value="' + color + ' "> ' + color + ' </option>';
+  });
+  console.log(Array.isArray(fetchProductData.colors));
+};
