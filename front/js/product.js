bc0d222e782165aaac967ff407ade1f307adca3a
vadym-loik <loikfit@gmail.com>
2022-09-26 17:47:18 +0200

    Update product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,25 +1,24 @@
+// get URL parameter values
 const queryString = window.location.search;
-console.log(queryString);
-
 const urlParams = new URLSearchParams(queryString);
-console.log(urlParams.has('id'));
-
 const productId = urlParams.get('id');
-console.log(productId);
 
+//taking products from the API with IDs
 fetch(`http://localhost:3000/api/products/${productId}`)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    renderProduct(data, document);
+    renderProduct(data);
+    productAddToLocalstorage(data);
     console.log(data);
   })
   .catch((error) => {
     console.log('Error ' + error);
   });
 
-const renderProduct = (fetchProductData, document) => {
+// take articles from the fetch and add them to the DOM
+const renderProduct = (fetchProductData) => {
   document.querySelector('.item__img').innerHTML =
     '<img class="prodImg" src="' +
     fetchProductData.imageUrl +
@@ -28,9 +27,32 @@ const renderProduct = (fetchProductData, document) => {
   document.querySelector('#price').innerText = fetchProductData.price;
   document.querySelector('#description').innerText =
     fetchProductData.description;
+
+  // forEach loop for the colours
   fetchProductData.colors.forEach((color) => {
     document.querySelector('#colors').innerHTML +=
       '<option value="' + color + ' "> ' + color + ' </option>';
   });
-  console.log(Array.isArray(fetchProductData.colors));
 };
+
+// add product to the localStorage
+function productAddToLocalstorage(fetchProductData) {
+  const addBtn = document.querySelector('#addToCart');
+
+  addBtn.addEventListener('click', () => {
+    let prodColour = document.querySelector('#colors').value;
+    let prodQuantity = document.querySelector('#quantity').value;
+
+    const myProd = {
+      img: fetchProductData.imageUrl,
+      name: fetchProductData.name,
+      id: fetchProductData._id,
+      colour: prodColour,
+      quantity: prodQuantity,
+    };
+
+    localStorage.setItem('myProd', JSON.stringify(myProd));
+    console.log(myProd);
+  });
+}
+// console.log(Array.isArray(fetchProductData.colors));
