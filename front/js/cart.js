d30c1cab9968eddb5396fbc76c5d411ee6999841
vadym-loik <loikfit@gmail.com>
2022-09-27 11:09:06 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -3,20 +3,23 @@ dataFromLocalstorage = JSON.parse(dataFromLocalstorage);
 
 const itemsCartSection = document.querySelector('#cart__items');
 
+let totalPrice = [];
+let totalQuantity = [];
+
 for (i = 0; i < dataFromLocalstorage.length; i += 1) {
-  itemsCartSection.innerHTML += `<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+  itemsCartSection.innerHTML += `<article class="cart__item" data-id="${dataFromLocalstorage[i].id}" data-color="${dataFromLocalstorage[i].colour}">
                 <div class="cart__item__img">
-                  <img src="../images/product01.jpg" alt="Photo of a sofa">
+                  <img src="${dataFromLocalstorage[i].img}" alt="Photo of a sofa">
                 </div>
                 <div class="cart__item__content">
                   <div class="cart__item__content__description">
-                    <h2>Name of the product</h2>
-                    <p>Green</p>
-                    <p>€42.00</p>
+                    <h2>${dataFromLocalstorage[i].name}</h2>
+                    <p>${dataFromLocalstorage[i].colour}</p>
+                    <p>${dataFromLocalstorage[i].price} + "€"</p>
                   </div>
                   <div class="cart__item__content__settings">
                     <div class="cart__item__content__settings__quantity">
-                      <p>Quantity : </p>
+                      <p>Quantity : ${dataFromLocalstorage[i].quantity}</p>
                       <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
                     </div>
                     <div class="cart__item__content__settings__delete">
@@ -26,6 +29,14 @@ for (i = 0; i < dataFromLocalstorage.length; i += 1) {
                 </div>
               </article>
     `;
+
+  let quantityNumber = parseInt(dataFromLocalstorage[i].quantity);
+  let priceNumber = parseInt(
+    productInLocalStorage[i].price * dataFromLocalstorage[i].quantity
+  );
+
+  totalQuantity.push(quantityNumber);
+  totalPrice.push(priceNumber);
 }
 
 console.log(dataFromLocalstorage);
