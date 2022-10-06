cf2089d19ef3687a3c89d6ad083ca7ea18215297
vadym-loik <loikfit@gmail.com>
2022-10-06 10:41:02 +0200

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,3 +2,14 @@ let dataFromLocalstorage = localStorage.getItem('myProd');
 dataFromLocalstorage = JSON.parse(dataFromLocalstorage);
 
 const itemsCartSection = document.querySelector('#cart__items');
+
+const myCartData = [];
+
+console.log(myCartData);
+
+function putDataToTheCard() {
+  myCartData.push(dataFromLocalstorage);
+  return myCartData;
+}
+
+putDataToTheCard();
