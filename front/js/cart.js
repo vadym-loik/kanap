35fe25ec15ff159c2985617f01c0b06b732dd650
vadym-loik <loikfit@gmail.com>
2022-11-02 20:11:16 +0100

    Update cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,58 +1,17 @@
 const itemsCartSection = document.querySelector('#cart__items');
 
-const totalPrice = document.querySelector('#totalPrice');
-
-const cartItemsLocal = JSON.parse(localStorage.getItem('cartItems')) || {};
-
 // find unique key in the cart object
-const cartItems = Object.keys(cartItemsLocal).map((key) => cartItemsLocal[key]);
-
-// add product to the localstorage
-export function addToCart(product) {
-  try {
-    const productMap = JSON.parse(localStorage.getItem('cartItems'));
-    const colour = document.querySelector('#colors').value;
-    const quantity = parseInt(document.querySelector('#quantity').value);
-    const newProduct = { ...product, colour, quantity };
-    const productKey = product._id + colour;
-
-    if (productMap[productKey]) {
-      productMap[productKey].quantity = quantity;
-    } else {
-      productMap[productKey] = newProduct;
-    }
-
-    window.localStorage.setItem('cartItems', JSON.stringify(productMap));
-  } catch (event) {
-    console.error(event);
-  }
+function getCartItems() {
+  const cartItemsLocal = JSON.parse(localStorage.getItem('cartItems')) || {};
+  return Object.keys(cartItemsLocal).map((key) => cartItemsLocal[key]);
 }
 
-// calculate the total quantity of the articles and total price
-const getTotalQuantity = async () => {
-  const quantity = document.querySelectorAll('.itemQuantity');
-  let totalQuantity = 0;
-  for (let i = 0; i < quantity.length; i++) {
-    let value = quantity[i].value;
-    totalQuantity += parseInt(value);
-  }
-  document.querySelector('#totalQuantity').innerHTML = totalQuantity;
-
-  // let totalPrice = 0;
-  // for (let i = 0; i < cartItemsLocal.length; i++) {
-  //   const productMap = JSON.parse(localStorage.getItem('cartItems'));
-  //   let item = cartItemsLocal[i];
-  //   productMap =
-
-  // }
-};
-
-// add products to the cart (render)
-cartItems.forEach((cartItem) => {
-  if (itemsCartSection) {
-    itemsCartSection.innerHTML += `<article class="cart__item" data-id="${
-      cartItem.id
-    }" data-color="${cartItem.colour}">
+//render products on the cart page
+const renderProductCart = (cartItems) => {
+  itemsCartSection.innerHTML = '';
+  cartItems.forEach((cartItem) => {
+    const datakey = cartItem._id + cartItem.colour;
+    itemsCartSection.innerHTML += `<article class="cart__item" data-id="${cartItem.id}" data-color="${cartItem.colour}">
               <div class="cart__item__img">
                 <img src="${cartItem.imageUrl}" alt="Photo of a sofa">
               </div>
@@ -65,43 +24,83 @@ cartItems.forEach((cartItem) => {
                 <div class="cart__item__content__settings">
                   <div class="cart__item__content__settings__quantity">
                     <p>Quantity : </p>
-                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${
-                      cartItem.quantity
-                    }">
+                    <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${cartItem.quantity}">
                   </div>
                   <div class="cart__item__content__settings__delete">
-                    <p class="deleteItem" data-key="${
-                      cartItem._id + cartItem.colour
-                    }">Delete</p>
+                    <p class="deleteItem" data-key="${datakey}">Delete</p>
                   </div>
                 </div>
               </div>
             </article>`;
-  }
-  getTotalQuantity();
-});
 
-//remove product from the cart
+    const deleteItems = document.querySelectorAll('.deleteItem');
+    deleteItems.forEach((deleteBtn) => {
+      addDeleteEventlistener(deleteBtn);
+    });
+
+    // sdelat' tut forEach dlya changeQuantity
+  });
+};
+renderProductCart(getCartItems());
+
+function addDeleteEventlistener(deleteItem) {
+  return deleteItem.addEventListener('click', (event) => {
+    const productId = event.target.dataset.key;
+
+    removeFromCart(productId);
+  });
+}
+
+//remove product from the cart function
 function removeFromCart(id) {
   try {
     const productMap = JSON.parse(localStorage.getItem('cartItems'));
+    // console.log(productMap);
 
     if (productMap[id]) {
       delete productMap[id];
     }
 
     window.localStorage.setItem('cartItems', JSON.stringify(productMap));
+    renderProductCart(getCartItems());
   } catch (event) {
     console.error(event);
   }
 }
 
-// remove product from the cart
-const deleteItem = document.querySelectorAll('.deleteItem');
-deleteItem.forEach((deleteBtn) => {
-  deleteBtn.addEventListener('click', (event) => {
-    const productId = event.target.dataset.key;
-    console.log(productId);
-    removeFromCart(productId);
-  });
-});
+// calculate the total quantity of the articles and total price
+const getTotalQuantity = () => {
+  let totalQuantity = 0;
+  let totalPrice = 0;
+  const cartItems = getCartItems();
+  for (let i = 0; i < cartItems.length; i++) {
+    let value = cartItems[i].quantity;
+    totalQuantity += parseInt(value);
+    let item = cartItems[i].price;
+    totalPrice += parseInt(item) * cartItems[i].quantity;
+  }
+  document.querySelector('#totalQuantity').innerHTML = totalQuantity;
+  document.querySelector('#totalPrice').innerHTML = totalPrice;
+};
+
+// change quantity function
+const changeQuantity = () => {
+  const inputBtn = document.querySelectorAll('.itemQuantity');
+  const cartItems = getCartItems();
+  for (let i = 0; i < cartItems.length; i++) {
+    inputBtn[i].addEventListener('change', (event) => {
+      event.preventDefault();
+
+      let modifiedValue = inputBtn[i].value;
+      console.log(modifiedValue);
+
+      if (modifiedValue > 0 && modifiedValue <= 100) {
+        cartItems[i].quantity = modifiedValue;
+        window.localStorage.setItem('cartItems', JSON.stringify(cartItems));
+      }
+    });
+  }
+
+  getTotalQuantity();
+};
+changeQuantity();
